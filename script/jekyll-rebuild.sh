7e1f5ddf83fa5bf62e452fdd3cee77ec0ce48c7e
Gabriel Krieshok <gabriel.krieshok@gmail.com>
2019-06-06 08:36:16 -0400

    Jekyll rebuild

--- script/jekyll-rebuild.sh
+++ script/jekyll-rebuild.sh
@@ -14,15 +14,15 @@ rm -rf _site
 mkdir _site
 
 # clone remote repo to "_site"
-git clone https://${GH_TOKEN}@github.com/YOUR_GITHUB_USERNAME/YOUR_GITHUB_REPO --branch master _site
+git clone https://${GH_TOKEN}@github.com/gabrielkrieshok/thesilentpodcast --branch master _site
 
 # build with Jekyll into "_site"
 # exec jekyll build
 
 # push empty commit
 cd _site
-git config user.email "your@email.com"
-git config user.name "Your_Username"
+git config user.email "gabriel.krieshok@gmail.com"
+git config user.name "gabrielkrieshok"
 git commit -a -m "rebuild pages" --allow-empty
 git push origin master
 
