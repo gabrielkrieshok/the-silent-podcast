2d9f59cff3e697358645188be29704314ce3dfe6
Gabriel Krieshok <gabriel.krieshok@gmail.com>
2020-07-18 14:50:17 -0400

    Travis CI updates

--- script/jekyll-rebuild.sh
+++ script/jekyll-rebuild.sh
@@ -14,7 +14,7 @@ rm -rf _site
 mkdir _site
 
 # clone remote repo to "_site"
-git clone https://${GH_TOKEN}@github.com/gabrielkrieshok/thesilentpodcast --branch master _site
+git clone https://${GH_TOKEN}@github.com/gabrielkrieshok/thesilentpodcast --branch main _site
 
 # build with Jekyll into "_site"
 # exec jekyll build
@@ -27,8 +27,8 @@ cd _site
 git config user.email "gabriel.krieshok@gmail.com"
 git config user.name "gabrielkrieshok"
 git commit -a -m "rebuild pages" --allow-empty
-git push origin master
+git push origin main
 
 # remove last empty commit
 git reset HEAD~
-git push origin master --force
+git push origin main --force
