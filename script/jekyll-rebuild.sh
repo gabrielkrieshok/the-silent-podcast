f1ed879b6bec974057977fd8c755e66914769cf5
Gabriel Krieshok <gabriel.krieshok@gmail.com>
2024-07-02 08:51:54 -0400

    Setting up for GitHub Actions daily build

--- script/jekyll-rebuild.sh
+++ script/jekyll-rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # skip if build is triggered by pull request
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
   echo "this is PR, exiting"
   exit 0
 fi
@@ -16,9 +16,9 @@ mkdir _site
 # clone remote repo to "_site"
 git clone https://${GH_TOKEN}@github.com/gabrielkrieshok/thesilentpodcast --branch main _site
 
-  # build with Jekyll into "_site"
-  # exec jekyll build
-  # bundle exec rake site:deploy
+# build with Jekyll into "_site"
+# exec jekyll build
+# bundle exec rake site:deploy
 
 # bundle exec rake post
 
